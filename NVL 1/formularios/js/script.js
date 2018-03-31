38fce14971734061550a243609a858a989ffcbbc
Christan RCSG <christian.floppy@gmail.com>
2018-03-31 16:07:39 -0600

    Verificacion de informacion
    Datos almacenados con JS

---
+++ NVL 1/formularios/js/script.js
@@ -0,0 +1,16 @@
+function verEsctructura() {
+	console.log(form.ejemplo1.value);
+	console.log(form.ejemplo2.value);
+	console.log(form.ejemplo3.value);
+	console.log(form.ejemplo4.value);
+}
+
+verificar(){
+	var formulario = document.querySelector("form");
+}
+if (form.log(form.eje.value)= "1") {
+	alert("Es numero");
+}
+else{
+	alert("No es numero");
+}
